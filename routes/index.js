663a767a3bfceee40257bfb6ea3f2dd72838a58b
Uwancha <sito2alemu@gmail.com>
2023-12-19 23:53:40 +0300

    feat: Add route to handle post resquests This commit enables sending messages handling post requests

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,36 @@ const messages = [
     text: "Hello World!",
     user: "Charles",
     added: new Date()
+  },
+  {
+    text: "Hi there!",
+    user: "Lula",
+    added: new Date()
+  },
+  {
+    text: "Heyyy",
+    user: "Chare",
+    added: new Date()
+  },
+  {
+    text: "Howdy",
+    user: "Lembo",
+    added: new Date()
+  },
+  {
+    text: "fine",
+    user: "Nati",
+    added: new Date()
+  },
+  {
+    text: "Fella",
+    user: "Abel",
+    added: new Date()
+  },
+  {
+    text: "hey gus",
+    user: "Betty",
+    added: new Date()
   }
 ];
 
@@ -18,5 +48,14 @@ const messages = [
 router.get('/', (req, res) => {
   res.render('index', { title: 'Mini MessageBoard', messages: messages });
 });
+
+/* Post new form */ 
+router.post('/new', (req, res) => {
+  const {messageUser, messageText } = req.body
+
+  messages.push({text: messageText, user: messageUser, added: new Date()});
+
+  res.redirect('/')
+})
 
 module.exports = router;
